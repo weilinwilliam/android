06006fb86f1e1b8af6cbc1f78735076ee1f94fc7
William Wei <weilinwilliam@gmail.com>
2014-11-10 18:39:26 +1100

    android learning

---
+++ Examples/AudioVideoRingtoneManager/gen/course/examples/AudioVideo/Ringtone/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package course.examples.AudioVideo.Ringtone;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
